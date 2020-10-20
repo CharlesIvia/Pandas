bbffd5e2503e4be8676f892515d32c3bf51a8409
Charles Ivia <iviacharles@gmail.com>
2020-10-20 17:03:36 +0300

    Dates and time_series data

--- Timeseries/time_series.py
+++ Timeseries/time_series.py
@@ -19,3 +19,52 @@ print(df.shape)
 
 test = df.loc[0, "Date"].day_name()
 print(test)
+
+# Datename of entire column/series
+
+date_names = df["Date"].dt.day_name()
+print(date_names)
+
+# Create new column
+
+df["DayOfWeek"] = date_names
+print(df)
+
+# Earliest and latest date
+
+earliest = df["Date"].min()
+most_recent = df["Date"].max()
+
+print(earliest)
+print(most_recent)
+
+# TimeDelta
+
+tm_delta = most_recent - earliest
+print(tm_delta)
+
+# Filters
+
+filt = df["Date"] >= "2020"
+print(df.loc[filt])
+
+filt = (df["Date"] >= "2019") & (df["Date"] < "2020")
+print(df.loc[filt])
+
+# Alternative filters
+
+filt = (df["Date"] >= pd.to_datetime("2019-01-01")) & (
+    df["Date"] < pd.to_datetime("2020-01-01")
+)
+print(df.loc[filt])
+
+# Set index to Date
+df.set_index("Date", inplace=True)
+print(df)
+
+# Data for 2019
+
+# data_2019 = df["2019"]
+# print(data_2019)
+
+# Data on daily basis
